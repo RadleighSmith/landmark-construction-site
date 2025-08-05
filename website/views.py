59931f2a5388bc994ba5486cb98db74aef7702ec
RadleighSmith <RadleighMSmith@gmail.com>
2025-08-05 18:21:05 +0100

    Set up page views, URLs, and templates for main site pages

--- website/views.py
+++ website/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'website/home.html')
+
+def about(request):
+    return render(request, 'website/about.html')
+
+def services(request):
+    return render(request, 'website/services.html')
+
+def gallery(request):
+    return render(request, 'website/gallery.html')
+
+def contact(request):
+    return render(request, 'website/contact.html')
